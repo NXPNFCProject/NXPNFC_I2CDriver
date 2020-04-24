25c57d3a9ed89449979b811ed02c8d2b6bdf7b76
Nanjesh <nanjesh.s_1@nxp.com>
2020-04-24 23:24:40 +0530

    Updated corresponding to - NFC_AR_00_6000_11.02.00_OpnSrc

--- pn553-i2c/cold_reset.h
+++ pn553-i2c/cold_reset.h
@@ -16,13 +16,24 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ******************************************************************************/
+#ifndef _NFC_COMMON_H_
+#define _NFC_COMMON_H_
 
-#ifndef _NFC_COLD_RESET_H_
-#define _NFC_COLD_RESET_H_
+#define MSG_NFCC_RSP              0x40
+#define MSG_PROP_GID              0x0F
+#define ESE_CLD_RST_OID           0x1E
+#define RST_PROTECTION_CMD_INDEX  0x03
+
+#define RST_PROTECTION_OID        0x1F
+#define RST_PROTECTION_ENABLED    0x08
 
 typedef enum ese_cold_reset_origin {
-    ESE_COLD_RESET_SOURCE_SPI,
-    ESE_COLD_RESET_SOURCE_UWB,
-} ese_cold_reset_origin_t;
+    ESE_COLD_RESET_NOT_REQUESTED = 0x00,
+    ESE_COLD_RESET_SOURCE_NFC = 0x01,
+    ESE_COLD_RESET_SOURCE_SPI = 0x02,
+    ESE_COLD_RESET_SOURCE_UWB = 0x04,
+}ese_cold_reset_origin_t;
 
-#endif /* NFC_COLD_RESET_H_ */
+void ese_reset_resource_init(void);
+void ese_reset_resource_destroy(void);
+#endif /* _NFC_COMMON_H_ */
